36e33da94b88a5ed562b06f58851282d6f5af4e6
7336x <R7664alhussaini@gmail.com>
2024-10-02 15:54:43 +0300

    intial commit

--- main.dart
+++ main.dart
@@ -1,7 +1,41 @@
 void main(){
   // Write your code here
+  List <int> integers = [5, 3,15 ,4 ,1];
+  int sum =0 ;
+  integers.forEach((elements)
+  {
+sum += elements;
+  } );
+  print("The sum of the elements is $sum");
+   
+
+   int largenum = integers[0];
+  integers.forEach((largest)
+  {
+     if (largest>largenum)
+     {
+      largenum = largest ;
+     } 
+  });
+print("The largest number are $largenum");
   
   
-  
-  
+ List<dynamic> ages = [10,22, 18 , 24];
+ int minAge=20;
+ List<dynamic>filterVisitors = ages.where((ages)=> ages > minAge).toList();
+ print(filterVisitors);
+
+ List<int>num =[3,6,11,15,20];
+ int firstOdd (List<int> num ){
+  return num.firstWhere((num) => num.isOdd,
+  orElse: () => 0, );
+}
+print(firstOdd([2,3,4,7]));
+
+ 
+ List<int> noOddNum = [ 2, 3 ,5,7 ,8 ];
+ int OddNum = noOddNum.firstWhere((number)=> number.isOdd, orElse: () => 0
+ );
+print(OddNum);
+
 }
